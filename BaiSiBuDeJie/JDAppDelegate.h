d7b70d982890e0d4562195beee53042c8b5e242c
JiangDi <JiangDi@JiangDi.local>
2016-06-01 08:45:32 +0800

    素材

---
+++ BaiSiBuDeJie/JDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JDAppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by JiangDi on 16/6/1.
+//  Copyright © 2016年 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaiSiBuDeJie/JDAppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  JDAppDelegate.m
+//  BaiSiBuDeJie
+//
+//  Created by JiangDi on 16/6/1.
+//  Copyright © 2016年 Google. All rights reserved.
+//
+
+#import "JDAppDelegate.h"
+
+@interface JDAppDelegate ()
+
+@end
+
+@implementation JDAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+@end
